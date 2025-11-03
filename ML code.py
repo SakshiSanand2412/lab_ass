352c534ce67d99377c2f212c5a82bf3b031d030e
Sakshi Sanand <sakshi.sanand@gmail.com>
2025-11-03 14:48:15 +0530

    created

---
+++ ML code.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from sklearn.linear_model import LinearRegression
+import numpy as np
+X = np.array([[1], [2], [3], [4], [5]])
+y = np.array([2, 4, 6, 8, 10])
+
+model = LinearRegression()
+model.fit(X, y)
+
+print("Model trained successfully!")
+print("Prediction for 4:", model.predict([[6]]))
+
+
+# In[ ]:
+
+
+
+
